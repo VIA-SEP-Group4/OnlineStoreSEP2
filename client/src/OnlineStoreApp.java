cbdd8f5e7e38833aa2787be192ce25a3b9147faa
Mike Abbey <mihaiavram72@gmail.com>
2021-11-03 13:57:20 +0100

    First Commit

---
+++ client/src/OnlineStoreApp.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OnlineStoreApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ client/src/RunOnlineStoreApp.java
@@ -0,0 +1,7 @@
+import javafx.application.Application;
+
+public class RunOnlineStoreApp {
+    public static void main(String[] args) {
+        Application.launch(OnlineStoreApp.class);
+    }
+}
