9e5a9066c255ce353e95789e86a7a90d425c6189
Emi-27 <emijos3@gmail.com>
2021-11-22 22:07:01 +0100

    Browser

--- client/src/View/Browser/BrowserViewController.java
+++ client/src/View/Browser/BrowserViewController.java
@@ -7,29 +7,24 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-
+import javafx.scene.layout.GridPane;
 
 public class BrowserViewController implements ViewController
 {
   public TextField searchTextField;
-  public Button addButton1;
-  public Button addButton2;
-  public Button addButton3;
-  public Button addButton4;
-  public Button addButton5;
-  public Button addButton6;
-  public Label productDescriptionText1;
-  public Label productDescriptionText2;
-  public Label productDescriptionText3;
-  public Label productDescriptionText4;
-  public Label productDescriptionText5;
-  public Label productDescriptionText6;
+  public Button addButton;
   public Label itemsLabel;
   public Pagination pagination;
   public ComboBox <String> filterByComboBox;
   public Button loginButton;
   public Button registerButton;
   public Label idLabel;
+  public Label nameProdLabel;
+  public Label typeLabel;
+  public Label descriptionLabel;
+  public Label priceLabel;
+  public Label quantityLabel;
+  public GridPane gridPaneBrowser;
   private ViewHandler viewHandler;
   private BrowserViewModel viewModel;
 
@@ -37,26 +32,19 @@ public class BrowserViewController implements ViewController
     this.viewHandler = viewHandler;
     this.viewModel = viewModelFactory.getBrowserViewModel();
     searchTextField.textProperty().bindBidirectional(viewModel.searchProperty());
-    productDescriptionText1.textProperty().bindBidirectional(viewModel.prodDescription1Property());
-    productDescriptionText2.textProperty().bindBidirectional(viewModel.prodDescription2Property());
-    productDescriptionText3.textProperty().bindBidirectional(viewModel.prodDescription3Property());
-    productDescriptionText4.textProperty().bindBidirectional(viewModel.prodDescription4Property());
-    productDescriptionText5.textProperty().bindBidirectional(viewModel.prodDescription5Property());
-    productDescriptionText6.textProperty().bindBidirectional(viewModel.prodDescription6Property());
     itemsLabel.textProperty().bindBidirectional(viewModel.itemsProperty());
     pagination.currentPageIndexProperty().bindBidirectional(viewModel.pageProperty());
     filterByComboBox.getItems().addAll("All products", "Type", "Availability");
     //filterByComboBox.valueProperty().bindBidirectional(viewModel.filterProperty());
-    viewModel.getProd();
-    viewModel.button1Property().addListener((obs, oldVal, newVal) -> addButton1.setDisable(newVal));
-    viewModel.button2Property().addListener((obs, oldVal, newVal) -> addButton2.setDisable(newVal));
-    viewModel.button3Property().addListener((obs, oldVal, newVal) -> addButton3.setDisable(newVal));
-    viewModel.button4Property().addListener((obs, oldVal, newVal) -> addButton4.setDisable(newVal));
-    viewModel.button5Property().addListener((obs, oldVal, newVal) -> addButton5.setDisable(newVal));
-    viewModel.button6Property().addListener((obs, oldVal, newVal) -> addButton6.setDisable(newVal));
+    nameProdLabel.textProperty().bindBidirectional(viewModel.nameProdProperty());
+    typeLabel.textProperty().bindBidirectional(viewModel.typeProperty());
+    descriptionLabel.textProperty().bindBidirectional(viewModel.prodDescriptionProperty());
+    priceLabel.textProperty().bindBidirectional(viewModel.priceProperty());
+    quantityLabel.textProperty().bindBidirectional(viewModel.quantityProperty());
     loginButton.visibleProperty().bind(viewModel.hideButtonsProperty());
     registerButton.visibleProperty().bind(viewModel.hideButtonsProperty());
     idLabel.textProperty().bind(viewModel.idPropertyProperty());
+    viewModel.getProd();
     reset();
   }
 
--- client/src/View/Browser/BrowserViewModel.java
+++ client/src/View/Browser/BrowserViewModel.java
@@ -11,20 +11,13 @@ public class BrowserViewModel implements PropertyChangeListener
 {
   private LoginModel model;
   private StringProperty search;
-  private StringProperty prodDescription1;
-  private StringProperty prodDescription2;
-  private StringProperty prodDescription3;
-  private StringProperty prodDescription4;
-  private StringProperty prodDescription5;
-  private StringProperty prodDescription6;
+  private StringProperty nameProd;
+  private StringProperty type;
+  private StringProperty prodDescription;
+  private StringProperty price;
+  private StringProperty quantity;
   private StringProperty items;
   private IntegerProperty page;
-  private BooleanProperty button1;
-  private BooleanProperty button2;
-  private BooleanProperty button3;
-  private BooleanProperty button4;
-  private BooleanProperty button5;
-  private BooleanProperty button6;
   private ObjectProperty<String> filter;
   private BooleanProperty hideButtons;
   private StringProperty idProperty;
@@ -33,18 +26,11 @@ public class BrowserViewModel implements PropertyChangeListener
   {
     this.model = model;
     search = new SimpleStringProperty();
-    prodDescription1 = new SimpleStringProperty();
-    prodDescription2 = new SimpleStringProperty();
-    prodDescription3 = new SimpleStringProperty();
-    prodDescription4 = new SimpleStringProperty();
-    prodDescription5 = new SimpleStringProperty();
-    prodDescription6 = new SimpleStringProperty();
-    button1 = new SimpleBooleanProperty(false);
-    button2 = new SimpleBooleanProperty(false);
-    button3 = new SimpleBooleanProperty(false);
-    button4 = new SimpleBooleanProperty(false);
-    button5 = new SimpleBooleanProperty(false);
-    button6 = new SimpleBooleanProperty(false);
+    nameProd = new SimpleStringProperty();
+    type = new SimpleStringProperty();
+    prodDescription = new SimpleStringProperty();
+    price = new SimpleStringProperty();
+    quantity = new SimpleStringProperty();
     items = new SimpleStringProperty("");
     page = new SimpleIntegerProperty();
     hideButtons = new SimpleBooleanProperty(true);
@@ -62,34 +48,29 @@ public class BrowserViewModel implements PropertyChangeListener
     return search;
   }
 
-  public StringProperty prodDescription1Property()
+  public StringProperty nameProdProperty()
   {
-    return prodDescription1;
+    return nameProd;
   }
 
-  public StringProperty prodDescription2Property()
+  public StringProperty typeProperty()
   {
-    return prodDescription2;
+    return type;
   }
 
-  public StringProperty prodDescription3Property()
+  public StringProperty prodDescriptionProperty()
   {
-    return prodDescription3;
+    return prodDescription;
   }
 
-  public StringProperty prodDescription4Property()
+  public StringProperty priceProperty()
   {
-    return prodDescription4;
+    return price;
   }
 
-  public StringProperty prodDescription5Property()
+  public StringProperty quantityProperty()
   {
-    return prodDescription5;
-  }
-
-  public StringProperty prodDescription6Property()
-  {
-    return prodDescription6;
+    return quantity;
   }
 
   public StringProperty itemsProperty()
@@ -107,36 +88,6 @@ public class BrowserViewModel implements PropertyChangeListener
     return model;
   }
 
-  public BooleanProperty button1Property()
-  {
-    return button1;
-  }
-
-  public BooleanProperty button2Property()
-  {
-    return button2;
-  }
-
-  public BooleanProperty button3Property()
-  {
-    return button3;
-  }
-
-  public BooleanProperty button4Property()
-  {
-    return button4;
-  }
-
-  public BooleanProperty button5Property()
-  {
-    return button5;
-  }
-
-  public BooleanProperty button6Property()
-  {
-    return button6;
-  }
-
   public BooleanProperty hideButtonsProperty()
   {
     return hideButtons;
@@ -149,67 +100,7 @@ public class BrowserViewModel implements PropertyChangeListener
 
   public void getProd(){
     ArrayList<Product> prod = model.getProducts(pageProperty().get());
-    if (prod != null)
-    {
-      if (prod.size() == 0)
-      {
-        pageProperty().setValue(1);
-        getProd();
-      }
-      this.prodDescription1.setValue(prod.get(0).toString());
-      if (prod.get(1) != null)
-      {
-        button2.setValue(false);
-        this.prodDescription2.setValue(prod.get(1).toString());
-      }
-      else
-      {
-        prodDescription2.setValue("");
-        button2.setValue(true);
-      }
-      if (prod.get(1) != null)
-      {
-        button3.setValue(false);
-        this.prodDescription3.setValue(prod.get(2).toString());
-      }
-      else
-      {
-        prodDescription3.setValue("");
-        button3.setValue(true);
-      }
-      if (prod.get(1) != null)
-      {
-        button4.setValue(false);
-        this.prodDescription4.setValue(prod.get(3).toString());
-      }
-      else
-      {
-        prodDescription4.setValue("");
-        button4.setValue(true);
-      }
-      if (prod.get(1) != null)
-      {
-        button5.setValue(false);
-        this.prodDescription5.setValue(prod.get(4).toString());
-      }
-
-      else
-      {
-        prodDescription5.setValue("");
-        button5.setValue(true);
-      }
-      if (prod.get(1) != null)
-      {
-        button6.setValue(false);
-        this.prodDescription6.setValue(prod.get(5).toString());
-      }
-      else
-      {
-        prodDescription6.setValue("");
-        button6.setValue(true);
-      }
     }
-  }
 
   public void addBasket(int id)
   {
