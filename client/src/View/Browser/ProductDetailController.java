b36ec1f44eb33d18aedff5e313089b181ba65b33
Emi-27 <emijos3@gmail.com>
2021-11-29 21:20:42 +0100

    Browser 3.0

---
+++ client/src/View/Browser/ProductDetailController.java
@@ -0,0 +1,33 @@
+package View.Browser;
+
+import Core.ViewHandler;
+import Core.ViewModelFactory;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class ProductDetailController
+{
+  public Label nameProd;
+  public Label typeProd;
+  public Label descriptionProd;
+  public Label priceProd;
+
+  private ViewHandler viewHandler;
+  private BrowserViewModel productDetailModel;
+
+  public void init(ViewHandler viewHandler) {
+    this.viewHandler = viewHandler;
+    this.productDetailModel = ViewModelFactory.getBrowserViewModel();
+
+    nameProd.textProperty().bind(productDetailModel.getSelectedProd().namePropertyProperty());
+    typeProd.textProperty().bind(productDetailModel.getSelectedProd().typePropertyProperty());
+    descriptionProd.textProperty().bind(productDetailModel.getSelectedProd().descriptionProperty());
+    priceProd.textProperty().bind(productDetailModel.getSelectedProd().pricePropertyProperty().asString());
+
+  }
+
+  public void onBack(ActionEvent actionEvent)
+  {
+    viewHandler.openBrowserPane();
+  }
+}
